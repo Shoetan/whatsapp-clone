ce3ae53e4d8653b08ff8447b750a9206ba0995c0
unknown <emmysoetan@gmail.com>
2022-09-09 04:49:41 +0100

    created the login page to sign In users with google

---
+++ components/Login.js
@@ -0,0 +1,68 @@
+import { Button } from '@mui/material';
+import Head from 'next/head';
+import styled from 'styled-components'
+
+
+const Login = () => {
+
+const signIn = () =>{
+
+}
+
+
+    return ( 
+        <Container>
+              <Head>
+                <title>Login</title>
+              </Head>
+
+              <LoginContainer>
+                <Logo
+                   src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png'
+                />
+
+                <LoginButton onClick={signIn}>Login with google</LoginButton>
+              </LoginContainer>
+        </Container>
+     );
+}
+ 
+export default Login;
+
+
+const Container = styled.div`
+   border: 2px solid black;
+   height:100vh;
+   display:grid;
+   place-items: center;
+   background-color: whitesmoke;
+`
+
+const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color:#ffffff;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+`
+
+const Logo = styled.img`
+ height:250px;
+ width:250px;
+ margin-bottom: 50px;
+
+`
+
+const LoginButton = styled(Button)`
+    background-color: #c1c1c1;
+    color: black;
+    padding: 5px;
+    margin : 5px;
+
+    :hover{
+        background-color: whitesmoke;
+        border: 1px solid black
+    }
+`
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { useAuthState} from 'react-firebase-hooks/auth'
+import Login from '../components/Login'
+import { auth , dataBase } from '../firebase'
+
+
 import '../styles/styles.css'
 function MyApp({ Component, pageProps }) {
+  const [user] = useAuthState(auth)
+
+  if (!user) return <Login/>
+
   return <Component {...pageProps} />
 }
 
